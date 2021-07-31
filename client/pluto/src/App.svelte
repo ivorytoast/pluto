86a03c8e8547f868f31d21ad9c9719251e4dd7b0
Anthony <anthonyhamill3853@gmail.com>
2021-07-31 00:36:12 -0400

    Just a client

--- client/pluto/src/App.svelte
+++ client/pluto/src/App.svelte
@@ -8,7 +8,37 @@
     import Lobby from "./routes/Lobby.svelte";
     import Stratego from "./routes/Stratego/Stratego.svelte";
     import Session from "./routes/Session.svelte";
+
+    let sessionId = "B1212390"
+
+    let URL = "http://proxy.titan-backend-nyc.com/"
+
+    // Get
+    let LATEST_BOARD = "/db/board/" // "/db/board/{id}"
+    let PLAYERS_IN_SESSION = "/sessions/players/" // "/sessions/players/{id}"
+
+    // Post
+    let UPDATE_BOARD = "/update/board"
+    let MOVE = "/move"
+    let NEW_SESSION = "/new/redis"
+    let JOIN = "/join"
+
+    const getLatestBoard = async (sessionId) => {
+        let urlToQuery = URL + LATEST_BOARD + sessionId;
+        const response = await fetch(urlToQuery)
+        console.log(response)
+    }
+
+    const getPlayersInSession = async (sessionId) => {
+        let urlToQuery = URL + PLAYERS_IN_SESSION + sessionId;
+        const response = await fetch(urlToQuery)
+        console.log(response)
+    }
 </script>
+
+<button on:click={getPlayersInSession(sessionId)}>
+    Players
+</button>
 
 <Router>
     <main>
--- server/README.md
+++
@@ -1,3 +0,0 @@
-Log In Design From: https://tailwindcomponents.com/component/login
-
-https://laracasts.com/series/alpine-essentials/episodes/11
